f2ab6c00a5c9cdf1ddfa6994b77c7ab310a16ec7
Ronny Bangsund <github@neuralshortcircuit.com>
2021-07-05 16:08:22 +0200

    Implemented key renaming.

--- cmd_rename.go
+++ cmd_rename.go
@@ -5,7 +5,14 @@
 
 package main
 
-import "github.com/Urethramancer/signor/opt"
+import (
+	"context"
+
+	"github.com/Urethramancer/signor/opt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
+)
 
 // RenameCmd options.
 type RenameCmd struct {
@@ -20,5 +27,71 @@ func (cmd *RenameCmd) Run(in []string) error {
 		return opt.ErrUsage
 	}
 
-	return nil
+	client, err := getClient()
+	if err != nil {
+		return nil
+	}
+
+	param, err := client.GetParameter(context.Background(), &ssm.GetParameterInput{
+		Name:           aws.String(validKey(cmd.Key)),
+		WithDecryption: true,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !askString("Are you sure you want to rename this parameter?") {
+		return nil
+	}
+
+	filter := types.ParameterStringFilter{
+		Key:    aws.String("Name"),
+		Option: aws.String("Equals"),
+		Values: []string{validKey(cmd.Key)},
+	}
+
+	dpi := ssm.DescribeParametersInput{
+		MaxResults:       25,
+		ParameterFilters: []types.ParameterStringFilter{filter},
+	}
+
+	desc, err := client.DescribeParameters(context.Background(), &dpi)
+	if err != nil {
+		return err
+	}
+
+	ppi := &ssm.PutParameterInput{
+		Name:        aws.String(validKey(cmd.New)),
+		Value:       param.Parameter.Value,
+		Type:        param.Parameter.Type,
+		Description: desc.Parameters[0].Description,
+		Overwrite:   true,
+	}
+	_, err = client.PutParameter(context.Background(), ppi)
+	if err != nil {
+		return err
+	}
+
+	tags, err := client.ListTagsForResource(context.Background(), &ssm.ListTagsForResourceInput{
+		ResourceId:   param.Parameter.Name,
+		ResourceType: types.ResourceTypeForTaggingParameter,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = client.AddTagsToResource(context.Background(), &ssm.AddTagsToResourceInput{
+		ResourceId:   aws.String(validKey(cmd.New)),
+		ResourceType: types.ResourceTypeForTaggingParameter,
+		Tags:         tags.TagList,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = client.DeleteParameter(context.Background(), &ssm.DeleteParameterInput{
+		Name: param.Parameter.Name,
+	})
+	return err
 }
